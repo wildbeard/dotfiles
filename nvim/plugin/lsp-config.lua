02332c182324996702e0c5c535c532ba3b97e4a8
Preston Haddock <quotablesarcasm@gmail.com>
2022-03-31 07:45:10 -0600

    Show eslint error code

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -71,7 +71,9 @@ local null_ls = require('null-ls')
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d.with({
+          diagnostics_format = "#{m} (#{s}: #{c})",
+        }),
         null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.formatting.eslint_d
    },
